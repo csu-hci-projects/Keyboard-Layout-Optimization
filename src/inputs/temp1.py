e417445f0d52c55982a1758d1f04f7bbefabc819
ethamCS <76605572+ethamCS@users.noreply.github.com>
2022-04-13 12:55:27 -0600

    detect type of controller input

--- src/inputs/temp1.py
+++ src/inputs/temp1.py
@@ -4,3 +4,12 @@ joysticks = []
 clock = pygame.time.Clock()
 keepPlaying = True
 
+# for al the connected joysticks
+for i in range(0, pygame.joystick.get_count()):
+    # create an Joystick object in our list
+    joysticks.append(pygame.joystick.Joystick(i))
+    # initialize them all (-1 means loop forever)
+    joysticks[-1].init()
+    # print a statement telling what the name of the controller is
+    print ("Detected joystick "),joysticks[-1].get_name(),"'"
+
