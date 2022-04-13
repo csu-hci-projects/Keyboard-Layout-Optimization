66d7e1df96fa135699b9456f3dc086565276b2ff
ethamCS <76605572+ethamCS@users.noreply.github.com>
2022-04-13 14:03:31 -0600

    create ctor

--- src/inputs/temp.py
+++ src/inputs/temp.py
@@ -3,17 +3,36 @@ import math
 import threading
 
 class XboxController(object):
+    MAX_TRIG_VAL = math.pow(2, 8)
+    MAX_JOY_VAL = math.pow(2, 15)
+
+    def __init__(self):
+
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+        self.RightJoystickY = 0
+        self.RightJoystickX = 0
+        self.LeftTrigger = 0
+        self.RightTrigger = 0
+        self.LeftBumper = 0
+        self.RightBumper = 0
+        self.A = 0
+        self.X = 0
+        self.Y = 0
+        self.B = 0
+        self.LeftThumb = 0
+        self.RightThumb = 0
+        self.Back = 0
+        self.Start = 0
+        self.LeftDPad = 0
+        self.RightDPad = 0
+        self.UpDPad = 0
+        self.DownDPad = 0
+
+
+
+
   
-
-
-
-
-    def _monitor_controller(self):
-        while True:
-            print('test')
-
-
-
 
 if __name__ == '__main__':
     joy = XboxController()
