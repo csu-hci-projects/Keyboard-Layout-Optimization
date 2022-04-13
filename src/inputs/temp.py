d044a470d5e1c480a47668e53fe9ddeeba62a787
ethamCS <76605572+ethamCS@users.noreply.github.com>
2022-04-13 14:04:41 -0600

    create read function

--- src/inputs/temp.py
+++ src/inputs/temp.py
@@ -34,7 +34,16 @@ class XboxController(object):
         self._monitor_thread.start()
 
 
-  
+    def read(self): # return the buttons/triggers that you care about in this methode
+        x = self.LeftJoystickX
+        y = self.LeftJoystickY
+        a = self.A
+        b = self.X # b=1, x=2
+        rb = self.RightBumper
+        return [x, y, a, b, rb]
+
+
+
 
 if __name__ == '__main__':
     joy = XboxController()
