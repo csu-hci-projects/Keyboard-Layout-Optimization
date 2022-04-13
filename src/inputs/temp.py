c880b1bdab12ae9f9fa0e046812a3c09406d5a79
ethamCS <76605572+ethamCS@users.noreply.github.com>
2022-04-13 14:01:59 -0600

    read the joy stick

--- src/inputs/temp.py
+++ src/inputs/temp.py
@@ -3,4 +3,19 @@ import math
 import threading
 
 class XboxController(object):
-    print('test')
+  
+
+
+
+
+    def _monitor_controller(self):
+        while True:
+            print('test')
+
+
+
+
+if __name__ == '__main__':
+    joy = XboxController()
+    while True:
+        print(joy.read())
