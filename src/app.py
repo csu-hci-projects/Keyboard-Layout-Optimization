66522b3892f1a7913940d099ae2bbc0f787b0514
ethamCS <76605572+ethamCS@users.noreply.github.com>
2022-04-14 01:07:25 -0600

    start GUI

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import client.kb1 as kb1
 import client.kb2 as kb2
 import client.kb3 as kb3
 import client.kb4 as kb4
+import client.kbstart as kbstart
 import transparency.kbT0 as kbT0
 import transparency.kbT1 as kbT1
 import transparency.kbT2 as kbT2
@@ -10,6 +11,8 @@ import transparency.kbT3 as kbT3
 import transparency.kbT4 as kbT4
 
 def start():
+    kbstart.GUI()
+
     kb0.GUI()
     
     kb1.GUI()
