b944d6fa0b7f971408db9fd2c72b6bb764f7a9ae
ethamCS <76605572+ethamCS@users.noreply.github.com>
2022-04-14 01:03:18 -0600

    display info needed for users

--- src/client/kbstart.py
+++ src/client/kbstart.py
@@ -14,4 +14,9 @@ def GUI():
     keyboardApp.geometry("800x500")
   
 
- 
+    readTextBox = Label(keyboardApp, text=testTxt1, font=('sans', 20))
+    readTextBox.grid(row=0, columnspan=11,ipadx=210, pady=20)
+
+    readTextBox = Label(keyboardApp, text=testTxt, font=('sans', 20))
+    readTextBox.grid(row=1, columnspan=11,ipadx=210, pady=20)
+    
